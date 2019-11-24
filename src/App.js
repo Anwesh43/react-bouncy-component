8afac57d6a055ace1a566e2d12be759edcef62aa
Anwesh43 <anweshthecool0@gmail.com>
2019-11-24 21:34:20 +0530

    adding src files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BouncyComponent from './BouncyComponent'
+
+function App() {
+  return (
+    <BouncyComponent>
+    </BouncyComponent>
+  );
+}
+
+export default App;
